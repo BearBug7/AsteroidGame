4a5940ffd656e1ca2b9df46f93483c55a2df095c
BearBug7 <niamhj577@gmail.com>
2020-06-18 17:49:37 +0000

    added code to move ship

--- main.py
+++ main.py
@@ -16,11 +16,37 @@ colour = (8, 127, 255)
 
 player = Ship((20, 200))
 
+NumLevels = 4
+Level = 1
+
 def main():
-  clock.tick(60)
-  screen.fill(colour)
-  screen.blit(player.image, player.rect)
-  pygame.display.flip()
+  global Level, NumLevels
+
+  while Level < NumLevels:
+    clock.tick(60)
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+          player.speed[0] = 10
+        if event.key == pygame.K_UP:
+          player.speed[1] = -10
+        if event.key == pygame.K_LEFT:
+          player.speed[0] = -10
+        if event.key == pygame.K_DOWN:
+          player.speed[1] = 10
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT:
+          player.speed[0] = 0
+        if event.key == pygame.K_UP:
+          player.speed[1] = 0
+        if event.key == pygame.K_LEFT:
+          player.speed[0] = 0
+        if event.key == pygame.K_DOWN:
+          player.speed[1] = 0
+    player.update()
+    screen.fill(colour)
+    screen.blit(player.image, player.rect)
+    pygame.display.flip()
 
 if __name__ == "__main__":
   main()
